c48fc795cceda5a81ed96346753d0fc2ae7b6423
christian-galito <christian.galito98@gmail.com>
2026-03-19 12:28:28 +0800

    add migration file for xmin row concurrency

---
+++ TeamTrack.Infrastructure/Persistence/Migrations/20260319042426_ModifiedBaseEntityXminColumn.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamTrack.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifiedBaseEntityXminColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<uint>(
+                name: "xmin",
+                table: "users",
+                type: "xid",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: 0u);
+
+            migrationBuilder.AddColumn<uint>(
+                name: "xmin",
+                table: "user_credentials",
+                type: "xid",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: 0u);
+
+            migrationBuilder.AddColumn<uint>(
+                name: "xmin",
+                table: "roles",
+                type: "xid",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: 0u);
+
+            migrationBuilder.AddColumn<uint>(
+                name: "xmin",
+                table: "projects",
+                type: "xid",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: 0u);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "xmin",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "xmin",
+                table: "user_credentials");
+
+            migrationBuilder.DropColumn(
+                name: "xmin",
+                table: "roles");
+
+            migrationBuilder.DropColumn(
+                name: "xmin",
+                table: "projects");
+        }
+    }
+}
